521ce81ea644093204ace12fc706d3e49699dcff
Ryo3250 <ryoman1002@icloud.com>
2026-01-23 19:49:14 +0900

    矢印の同化完了

--- Assets/DragAim.cs
+++ Assets/DragAim.cs
@@ -7,6 +7,14 @@ public class DragAim : MonoBehaviour
     [Header("参照")]
     [SerializeField] private Transform playerTransfrom;
 
+    [Header("矢印")]
+    [SerializeField] private Transform arrowTransform;
+    private SpriteRenderer arrowRenderer;
+
+    [Header("矢印のサイズ")]
+    [SerializeField] private float minArrowScale = 0.2f;
+    [SerializeField] private float maxArrowScale = 0.6f;
+
     [Header("引っ張り設定")]
     [SerializeField] private float maxDragDistance = 3f;
 
@@ -22,19 +30,24 @@ public class DragAim : MonoBehaviour
         line= GetComponent<LineRenderer>();
         line.positionCount = 2;
         line.enabled = false;
+
+        arrowRenderer = arrowTransform.GetComponent<SpriteRenderer>();
+        arrowTransform.gameObject.SetActive(false);
     }
 
     void Update()
     {
         //ボタンが押されたら
         if (Input.GetMouseButtonDown(0)) {
-            //startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
             line.enabled = true;
+            arrowTransform.gameObject.SetActive(true);
         }
 
         //押しているあいだは(引っ張り中)
         if (Input.GetMouseButton(0)) 
         {
+
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             Vector2 playerPos = playerTransfrom.position;
@@ -49,13 +62,25 @@ public class DragAim : MonoBehaviour
             line.SetPosition(0, playerPos);
             line.SetPosition(1, playerPos + clampedDir);
 
+            // 矢印の位置（線の先端）
+            arrowTransform.position = playerPos + clampedDir;
+
+            // 矢印の向き（飛ぶ方向を向かせる）
+            float angle = Mathf.Atan2(clampedDir.y, clampedDir.x) * Mathf.Rad2Deg -90f;
+            arrowTransform.rotation = Quaternion.Euler(0, 0, angle);
+
+
             //長さに応じて色を変える
             float t = clampedDir.magnitude / maxDragDistance;
-            Color lineColor = Color.Lerp(weakColor, strongColor, t);
+            Color line_aimColor = Color.Lerp(weakColor, strongColor, t);
+            //矢印の色を変える
+            float arrowScale = Mathf.Lerp(minArrowScale, maxArrowScale, t);
+            arrowTransform.localScale = new Vector3(arrowScale, arrowScale, 1f);
 
             //最大距離で制限
-            line.startColor = lineColor;
-            line .endColor = lineColor;
+            line.startColor = line_aimColor;
+            line .endColor = line_aimColor;
+            arrowRenderer.color = line_aimColor;
 
             
         }
@@ -64,6 +89,7 @@ public class DragAim : MonoBehaviour
         if (Input.GetMouseButtonUp(0)) 
         {
             line.enabled = false;
+            arrowTransform.gameObject.SetActive(false);
         }
     }
 }
