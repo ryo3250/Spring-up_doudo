fac9b6b6e104607f61e65f447264edd4170eb7f7
ryo3250 <ryoman1002@icloud.com>
2026-01-30 01:35:38 +0900

    条件付きクリア完成

--- Assets/test_Goal.cs
+++ Assets/test_Goal.cs
@@ -6,7 +6,7 @@ public class test_Goal : MonoBehaviour
     {
         if (other.CompareTag("Player")) 
         {
-            test_Stage_Manager.Instance.StageClear();
+            test_Stage_Manager.Instance.TryStageClear();
         }
     }
 }
--- Assets/test_Stage_Manager.cs
+++ Assets/test_Stage_Manager.cs
@@ -5,16 +5,112 @@ public class test_Stage_Manager : MonoBehaviour
 {
     public static test_Stage_Manager Instance;
 
-    [Header("ステージ設定")]
-    [SerializeField] private int stageMaxHitCount = 3;
+    [SerializeField] private Player3 player;
+    [SerializeField] private int maxHitCount = 3;
+    [SerializeField] private Text hitText;
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private GameObject goalUI;
+
+    private int hitCount;
+    private bool isGameOver;
+
+    void Awake()
+    {
+        Instance = this;
+    }
 
     void Start()
     {
-        test_Game_Manager.Instance.SetMaxHitCount(stageMaxHitCount);
+        InitializeStage();
     }
 
-    public void StageClear() 
+    void InitializeStage()
     {
-        test_Game_Manager.Instance.ShowGoalUI();
+        hitCount = 0;
+        isGameOver = false;
+        Time.timeScale = 1f;
+
+        if (player != null)
+        {
+            player.Initialize();
+        }
+
+        if (gameOverUI)
+        {
+            gameOverUI.SetActive(false);
+        }
+
+        if (goalUI)
+        {
+            goalUI.SetActive(false);
+        }
     }
+
+    public void AddHitCount()
+    {
+        if (isGameOver) return;
+
+        hitCount++;
+        Update();
+
+        if (hitCount > maxHitCount)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        if (isGameOver) return;
+
+        isGameOver = true;
+        Time.timeScale = 0f;
+
+        if (gameOverUI != null)
+        {
+            gameOverUI.SetActive(true);
+        }
+    }
+
+    public void TryStageClear()
+    {
+        if (isGameOver) return;
+
+        if (hitCount == maxHitCount)
+        {
+            StageClear();
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    public void StageClear()
+    {
+        if (isGameOver)
+        {
+            return;
+        }
+
+        isGameOver = true;
+        Time.timeScale = 0f;
+
+        if (goalUI != null)
+        {
+            goalUI.SetActive(true);
+        }
+
+    }
+
+    void Update()
+    {
+        if (hitText)
+        {
+            hitText.text = hitCount + "/" + maxHitCount;
+        }
+    }
+
+    public int GetHitCount() => hitCount;
+    public int GetMaxHitCount() => maxHitCount;
 }
